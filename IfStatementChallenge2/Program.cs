002ee28b4be30f9ba9f50088f8102a455170af40
RokRe <rokremic1@gmail.com>
2022-02-20 12:30:19 +0100

    Add project files.

---
+++ IfStatementChallenge2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IfStatementChallenge2
+{
+    internal class Program
+    {
+        static int highscore = 300;
+        static string highscorePlayer = "Rok";
+        static int score;
+        static string playerName;
+        static string scoreLevel;
+
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("Current highscore holder is {0} with {1} points", highscorePlayer, highscore);
+                Game();
+                Console.ReadKey();
+            }
+           
+            
+        }
+        public static void Game()
+        {
+            Console.WriteLine("\nNew game\nEnter your name:");
+            playerName= Console.ReadLine();
+            Console.WriteLine("What was your game score?");
+            score = int.Parse(Console.ReadLine());
+
+            if(score > highscore)
+            {
+                highscore = score;
+                highscorePlayer = playerName;
+                scoreLevel= score < 300 ? "bad" : (score < 1000 && score >= 300) ? "good" : "godlike";
+                Console.WriteLine("New highscore holder is {0} \nHighscore is now {1} points \nYour score is {2}\n", playerName, score, scoreLevel);
+            }
+            else
+            {
+                scoreLevel = score < 300 ? "bad" : (score < 1000 && score >= 300) ? "good" : "godlike";
+                Console.WriteLine("The old highscore of {0} made by {1} could not be broken \nYour score is {2}\n", highscore, highscorePlayer,scoreLevel);
+            }
+        }
+    }
+}
